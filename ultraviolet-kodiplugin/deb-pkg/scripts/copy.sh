7376395e3b69f59c9ada9270521741143ea2220a
ultravioletemulator <ultraviolet.emulator@gmail.com>
2015-10-20 15:39:25 +0200

    · Add copyright information

--- ultraviolet-kodiplugin/deb-pkg/scripts/copy.sh
+++ ultraviolet-kodiplugin/deb-pkg/scripts/copy.sh
@@ -4,8 +4,9 @@ rm -rf ./usr/share/man/*
 mkdir -p ./usr/lib/python3.4
 cp -r /home/developer/python/workspace/ultraviolet-emulator/git/ultraviolet-kodi-plugin/ultraviolet-kodiplugin/src/* ./usr/lib/python3.4/
 
-# mkdir ./usr/share
-# mkdir ./usr/share/man
+mkdir -p ./usr/share/doc/ultraviolet
+cp /home/developer/python/workspace/ultraviolet-emulator/git/ultraviolet-kodi-plugin/ultraviolet-kodiplugin/deb-pkg/doc/copyright ./usr/share/doc/ultraviolet/copyright
+
 mkdir -p ./usr/share/man/man1
 cp /home/developer/python/workspace/ultraviolet-emulator/git/ultraviolet-kodi-plugin/ultraviolet-kodiplugin/deb-pkg/man/ultraviolet.1 ./usr/share/man/man1/ultraviolet.1
 find ./ -type d | xargs chmod 755
--- ultraviolet-kodiplugin/deb-pkg/scripts/install.sh
+++ ultraviolet-kodiplugin/deb-pkg/scripts/install.sh
@@ -2,7 +2,8 @@
 sudo dpkg -r ultraviolet
 sudo dpkg -i ultraviolet-0.1-alpha.deb
 
-
+echo "Check license"
+dpkg-deb --fsys-tarfile ultraviolet-0.1-alpha.deb |tar -xvO ./usr/share/doc/ultraviolet/copyright
 
 
 
