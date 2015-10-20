ec0e4f68d1c702390846e1dc8501de93a93b3132
ultravioletemulator <ultraviolet.emulator@gmail.com>
2015-10-20 16:19:14 +0200

    · Rename commands and use shebangs

--- ultraviolet-kodiplugin/deb-pkg/scripts/install.sh
+++ ultraviolet-kodiplugin/deb-pkg/scripts/install.sh
@@ -6,4 +6,5 @@ echo "Check license"
 dpkg-deb --fsys-tarfile ultraviolet-0.1-alpha.deb |tar -xvO ./usr/share/doc/ultraviolet/copyright
 
 
-
+echo "http://www.aptly.info/"
+echo "Probatu aptly debian-en publikatzeko..."
