2522533f6f13f907fb920f87ded8de4522173b08
ultravioletemulator <ultraviolet.emulator@gmail.com>
2015-10-21 14:25:53 +0200

    · Refactor

---
+++ ultraviolet-kodiplugin/src/ultraviol/main.python.py
@@ -0,0 +1,13 @@
+__author__ = 'developer'
+import xbmc, xbmcgui
+
+
+class MyClass(xbmcgui.Window):
+    print ('hello world')
+
+
+
+    mydisplay = MyClass()
+mydisplay .doModal()
+del mydisplay
+
